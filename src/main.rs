ee595b73609696cd7e7a8ef0e1ad03dcb9566909
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-08-19 14:07:42 +0200

    fix: remove dead code

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,6 @@ trait RSSSource {
     async fn get(&self) -> Result<Vec<Item>, anyhow::Error>;
 }
 
-// Make our own error that wraps `anyhow::Error`.
-struct AppError(anyhow::Error);
-
-// Tell axum how to convert `AppError` into a response.
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
-    }
-}
-
-// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
-// `Result<_, AppError>`. That way you don't need to do that manually.
-impl<E> From<E> for AppError
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
-}
-
 async fn get_channel() -> Result<rss::Channel, Box<dyn std::error::Error>> {
     let cso_source = CSOSource::default();
     let darkreading_source = DarkReadingSource::default();
